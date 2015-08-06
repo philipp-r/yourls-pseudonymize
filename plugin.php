ad5ef5383ad03d44c2e66ecaecce92650a6105b7
Philipp <philipp-r@users.noreply.github.com>
2015-08-06 17:10:37 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,13 +1,16 @@
 <?php
 /*
 Plugin Name: Pseudonymize Plugin
-Plugin URI: http://github.com/ubicoo/yourls-pseudonymize
-Description: Pseudonymize IP addresses (remove last segment). Supports IPv4 and IPv6.
-Version: 1.1
+Plugin URI: http://github.com/philipp-r/yourls-pseudonymize
+Description: Pseudonymize IP addresses (remove last 2 segment). Supports IPv4 and IPv6.
+Version: 2
+Author: Philipp
+Author URI: http://github.com/philipp-r/
+ */
+/* Forked from:
 Author: Ubicoo
 Author URI: http://www.ubicoo.com
- */
-
+*/
 
 yourls_add_filter( 'get_IP', 'ubicoo_pseudonymize_IP' );
 
@@ -15,6 +18,7 @@ function ubicoo_pseudonymize_IP( $ip ) {
     if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
         $segments = explode(":", $ip);
         $segments[count($segments)-1] = 0;
+        $segments[count($segments)-2] = 0;
 
         $pseudo_IP = implode(":", $segments);
 
@@ -23,6 +27,7 @@ function ubicoo_pseudonymize_IP( $ip ) {
     } elseif(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
         $segments = explode(".", $ip);
         $segments[3] = 0;
+        $segments[2] = 0;
 
         $pseudo_IP = implode(".", $segments);
     } else {
